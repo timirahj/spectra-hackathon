c3ec8b6af0651184f4a32fb6acd40e3d5c46a619
timirahj <timirah.james@gmail.com>
2018-07-28 01:42:20 -0700

    Initial Commit

---
+++ FilterFun_SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilterFun_SampleApp
+//
+//  Created by Timirah James on 7/28/18.
+//  Copyright © 2018 Timirah James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
